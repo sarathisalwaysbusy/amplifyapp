cd4f3c6621992dc07f58e73386bcee0972217268
Sarath <tssarath@amazon.com>
2020-05-17 20:09:35 +0530

    Remove the link from homepage

--- src/App.js
+++ src/App.js
@@ -10,15 +10,6 @@ function App() {
         <p>
           hola amigos! this is my first react app!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
